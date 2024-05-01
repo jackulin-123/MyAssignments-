8651d79a3e8086c1cbbfb47c1abf15bb56e89c1a
jackulin-123 <168457101+jackulin-123@users.noreply.github.com>
2024-05-01 19:51:40 +0800

    Add files via upload
    Week1  Day1

---
+++ weeK1/day1/Firefox.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		int releaseYear =1998;
+	    String browserName = "firefox";
+		float browserVersion =100.2f;
+		char browserLogo = 'k';
+				
+		boolean isVisible = true;
+		System.out.println("Browser Version: "+browserVersion);
+		System.out.println("Browser Name: " +browserName);
+		System.out.println("Browser Logo: " +browserLogo);
+		System.out.println("Visible: " +isVisible);
+		System.out.println("Release year: " +releaseYear);
+		
+		
+		
+		
+	}
+
+}
---
+++ weeK1/day1/LearnDataType.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public class LearnDataType {
+	public static void main(String[] args) {
+		int age =30;
+		
+	}
+
+}
